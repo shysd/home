eec335224770aa7a46bc3320124004f38298f685
Shreyas <shreyasdethe9@gmail.com>
2022-07-01 03:53:36 +0800

    Overhaul 2022

--- js/script.js
+++ js/script.js
@@ -1,39 +1,38 @@
-let CURRENT_PAGE = 0;
+/* Global constants */
+let DARK_BRIGHT_MODE = "dark-mode";
+let COLOR_PRIMARY_BRIGHT = "#E0E0E0";
+let COLOR_PRIMARY_DARK = "#111111";
 
-let scrolling = false;
-let timer;
-let currentScrollPosition = window.scrollY;
+let BLINK_STATUS = true;
 
-document.addEventListener("scroll", scroll, false);
 
-function scroll() {
-    // console.log("Fired!");
-    document.removeEventListener("scroll", scroll, false);
-
-    window.setTimeout(doScroll, 100);
-
-    window.setTimeout(() => {
-        document.addEventListener("scroll", scroll, false);
-        currentScrollPosition = window.scrollY;
-    }, 750);
-
+/* Functions */
+const BodyLoaded = () => {
+    DarkBrightModeToggle();
+    BlinkingCharacter();
 }
 
-function doScroll() {
-    if (window.scrollY > currentScrollPosition) {
-        CURRENT_PAGE++;
-        // move down
-        scrollTo({
-            top: CURRENT_PAGE * window.visualViewport.height,
-            behavior: "smooth"
-        });
+// Toggles viewing mode, callback function for viewing mode button toggle
+const DarkBrightModeToggle = () => {
+    let _div = document.getElementById('dark-bright-mode-span');
+    if (DARK_BRIGHT_MODE == "dark-mode") {
+        _div.style.backgroundColor = COLOR_PRIMARY_BRIGHT;
+        _div.innerHTML = "&#9680;";  // Dark mode moon
+        DARK_BRIGHT_MODE = "bright-mode"
     }
     else {
-        CURRENT_PAGE--;
-        // move up
-        scrollTo({
-            top: CURRENT_PAGE * window.visualViewport.height,
-            behavior: "smooth"
-        });
+        _div.style.backgroundColor = COLOR_PRIMARY_DARK
+        _div.innerHTML = "&#9728;";  // Bright mode sun
+        DARK_BRIGHT_MODE = "dark-mode";
     }
+
+    document.body.classList.toggle("dark-mode");
+}
+
+const BlinkingCharacter = () => {
+    document.getElementById("blinking-character");
+    setInterval(() => {
+        BLINK_STATUS = !BLINK_STATUS;
+        document.getElementById("blinking-character").innerHTML = BLINK_STATUS ? "&#9619" : "";
+    }, 500)
 }
